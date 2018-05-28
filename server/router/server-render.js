fa3c7402ceb90ba716c1b8135ed1a0e3dd8071fc
xhx <1556423630@qq.com>
2018-05-28 21:52:42 +0800

    增加代码注释

--- server/router/server-render.js
+++ server/router/server-render.js
@@ -4,9 +4,11 @@ module.exports = async (context, renderer, template) => {
   const ctx = {url: context.path}
 
   try {
+    // 获取渲染后的应用字符串
     const appString = await renderer.renderToString(ctx)
-
+    // 读取server-entry里面设置的meta信息
     const { title } = ctx.meta.inject()
+    // 将数据渲染到页面模板里面 返回最终的html
     const html = ejs.render(template, {
       title: title.text(),
       appString,
